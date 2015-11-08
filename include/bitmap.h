0736443ce3ac3f585ce2b6271555b7bafc48d7cd
Zireael <Zireael-N@users.noreply.github.com>
2015-11-08 19:06:29 +0300

    Change indentation

--- include/bitmap.h
+++ include/bitmap.h
@@ -14,48 +14,48 @@
 // taken from wingdi.h
 namespace PNGStego {
 
-	enum Compression {
-		BI_RGB,
-		BI_RLE8,
-		BI_RLE4,
-		BI_BITFIELDS,
-		BI_JPEG,
-		BI_PNG
-	};
+enum Compression {
+	BI_RGB,
+	BI_RLE8,
+	BI_RLE4,
+	BI_BITFIELDS,
+	BI_JPEG,
+	BI_PNG
+};
 
 #pragma pack(push, 2)
-	struct BITMAPFILEHEADER {
-		uint16_t bfType;
-		uint32_t bfSize;
-		uint16_t bfReserved1;
-		uint16_t bfReserved2;
-		uint32_t bfOffBits;
-	};
-	struct BITMAPINFOHEADER {
-		uint32_t biSize;
-		int32_t biWidth;
-		int32_t biHeight;
-		uint16_t biPlanes;
-		uint16_t biBitCount;
-		uint32_t biCompression;
-		uint32_t biSizeImage;
-		int32_t biXPelsPerMeter;
-		int32_t biYPelsPerMeter;
-		uint32_t biClrUsed;
-		uint32_t biClrImportant;
-	};
+struct BITMAPFILEHEADER {
+	uint16_t bfType;
+	uint32_t bfSize;
+	uint16_t bfReserved1;
+	uint16_t bfReserved2;
+	uint32_t bfOffBits;
+};
+struct BITMAPINFOHEADER {
+	uint32_t biSize;
+	int32_t biWidth;
+	int32_t biHeight;
+	uint16_t biPlanes;
+	uint16_t biBitCount;
+	uint32_t biCompression;
+	uint32_t biSizeImage;
+	int32_t biXPelsPerMeter;
+	int32_t biYPelsPerMeter;
+	uint32_t biClrUsed;
+	uint32_t biClrImportant;
+};
 #pragma pack(1)
-	struct RGBTRIPLE {
-		uint8_t rgbtBlue;
-		uint8_t rgbtGreen;
-		uint8_t rgbtRed;
-	};
-	struct RGBQUAD {
-		uint8_t rgbBlue;
-		uint8_t rgbGreen;
-		uint8_t rgbRed;
-		uint8_t rgbReserved;
-	};
+struct RGBTRIPLE {
+	uint8_t rgbtBlue;
+	uint8_t rgbtGreen;
+	uint8_t rgbtRed;
+};
+struct RGBQUAD {
+	uint8_t rgbBlue;
+	uint8_t rgbGreen;
+	uint8_t rgbRed;
+	uint8_t rgbReserved;
+};
 #pragma pack(pop)
 
 } // namespace PNGStego
