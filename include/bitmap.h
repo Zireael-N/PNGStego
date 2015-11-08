daeca711d69e0abdf9f6262682f4de3451a203f4
Zireael <Zireael-N@users.noreply.github.com>
2015-11-08 19:04:16 +0300

    Placeholder for BMP-support

---
+++ include/bitmap.h
@@ -0,0 +1,62 @@
+//
+// Copyright (C) 2015 Zireael (zireael dot nk at gmail dot com)
+//  Distributed under the Boost Software License, Version 1.0.
+//       (See accompanying file LICENSE.md or copy at
+//           http://www.boost.org/LICENSE_1_0.txt)
+//
+
+#ifndef __BITMAP_H
+#define __BITMAP_H
+
+#include <cstdint>
+
+// https://en.wikipedia.org/wiki/BMP_file_format
+// taken from wingdi.h
+namespace PNGStego {
+
+	enum Compression {
+		BI_RGB,
+		BI_RLE8,
+		BI_RLE4,
+		BI_BITFIELDS,
+		BI_JPEG,
+		BI_PNG
+	};
+
+#pragma pack(push, 2)
+	struct BITMAPFILEHEADER {
+		uint16_t bfType;
+		uint32_t bfSize;
+		uint16_t bfReserved1;
+		uint16_t bfReserved2;
+		uint32_t bfOffBits;
+	};
+	struct BITMAPINFOHEADER {
+		uint32_t biSize;
+		int32_t biWidth;
+		int32_t biHeight;
+		uint16_t biPlanes;
+		uint16_t biBitCount;
+		uint32_t biCompression;
+		uint32_t biSizeImage;
+		int32_t biXPelsPerMeter;
+		int32_t biYPelsPerMeter;
+		uint32_t biClrUsed;
+		uint32_t biClrImportant;
+	};
+#pragma pack(1)
+	struct RGBTRIPLE {
+		uint8_t rgbtBlue;
+		uint8_t rgbtGreen;
+		uint8_t rgbtRed;
+	};
+	struct RGBQUAD {
+		uint8_t rgbBlue;
+		uint8_t rgbGreen;
+		uint8_t rgbRed;
+		uint8_t rgbReserved;
+	};
+#pragma pack(pop)
+
+} // namespace PNGStego
+#endif
